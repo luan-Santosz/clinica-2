d7d300826586de54734314c07fb5cf293a48a823
Nicolasjosafa <147561775+Nicolasjosafa@users.noreply.github.com>
2024-05-29 14:51:08 -0300

    ConsultaService.java

--- clinicaVeterinaria/src/Service/ConsultaService.java
+++ clinicaVeterinaria/src/Service/ConsultaService.java
@@ -1,37 +1,48 @@
-package Service;
+    package Service;
 
-import Model.Consulta;
-import java.util.HashMap;
-import java.util.Map;
+    import Model.Consulta;
+    import java.util.HashMap;
+    import java.util.Map;
 
-public class ConsultaService {
-    
-    private Map<Integer, Consulta> consultaMap = new HashMap<>();
-    
-    public void marcarConsulta(Consulta consulta) {
-        consultaMap.put(consulta.getId(), consulta);
-        System.out.println("Consulta marcada com sucesso.");
-    }
-    
-    public Consulta buscarConsulta(int id) {
-        return consultaMap.get(id);
-    }
-    
-    public void atualizarConsulta(Consulta consulta) {
-        if (consultaMap.containsKey(consulta.getId())) {
+    public class ConsultaService {
+        
+        private Map<Integer, Consulta> consultaMap = new HashMap<>();
+        
+        public void marcarConsulta(Consulta consulta) {
             consultaMap.put(consulta.getId(), consulta);
-            System.out.println("Consulta atualizada com sucesso.");
-        } else {
-            System.out.println("Consulta com ID " + consulta.getId() + " não encontrada.");
+            System.out.println("Consulta marcada com sucesso.");
         }
-    }
-    
-    public void excluirConsulta(int id) {
-        if (consultaMap.containsKey(id)) {
-            consultaMap.remove(id);
-            System.out.println("Consulta excluída com sucesso.");
-        } else {
-            System.out.println("Consulta com ID " + id + " não encontrada.");
+        
+        public Consulta buscarConsulta(int id) {
+            return consultaMap.get(id);
         }
+        
+        public void atualizarConsulta(Consulta consulta) {
+            if (consultaMap.containsKey(consulta.getId())) {
+                consultaMap.put(consulta.getId(), consulta);
+                System.out.println("Consulta atualizada com sucesso.");
+            } else {
+                System.out.println("Consulta com ID " + consulta.getId() + " não encontrada.");
+            }
+        }
+        
+        public void excluirConsulta(int id) {
+            if (consultaMap.containsKey(id)) {
+                consultaMap.remove(id);
+                System.out.println("Consulta excluída com sucesso.");
+            } else {
+                System.out.println("Consulta com ID " + id + " não encontrada.");
+            }
+        }
+
+        public int gerarNovoId() {
+            int maiorId = 0;
+            for (Integer id : consultaMap.keySet()) {
+                if (id > maiorId) {
+                    maiorId = id;
+                }
+            }
+            return maiorId + 1;
+        }
+        
     }
-}
