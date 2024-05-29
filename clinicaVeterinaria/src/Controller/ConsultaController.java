f37d80f7dad2de2c25037179f7478579c01ab211
Nicolasjosafa <147561775+Nicolasjosafa@users.noreply.github.com>
2024-05-29 14:44:43 -0300

    ConsultaController.java

--- clinicaVeterinaria/src/Controller/ConsultaController.java
+++ clinicaVeterinaria/src/Controller/ConsultaController.java
@@ -1,52 +1,44 @@
 package Controller;
 
-import java.sql.Date;
-
 import Model.Consulta;
 import Service.ConsultaService;
+import java.sql.Date;
 
 public class ConsultaController {
+    private final ConsultaService consultaService;
 
-    private final ConsultaService ConsultaService;
-
-    /**
-     * @param consultaService
-     */
     public ConsultaController(ConsultaService consultaService) {
-        this.ConsultaService = consultaService;
+        this.consultaService = consultaService;
     }
 
-    public void marcarConsulta(int id, int idAnimal, int idVeterinario, Date data) {
+    public void marcarConsulta(int idAnimal, int idVeterinario, Date data) {
+        // Gera um novo ID para a consulta. Pode ser melhorado com um gerador de ID mais sofisticado.
+        int id = consultaService.gerarNovoId();
         Consulta novaConsulta = new Consulta(id, idAnimal, idVeterinario, data, false);
-        ConsultaService.marcarConsulta(novaConsulta);
+        consultaService.marcarConsulta(novaConsulta);
     }
 
     public Consulta buscarConsulta(int id) {
-        return ConsultaService.buscarConsulta(id);
+        return consultaService.buscarConsulta(id);
     }
 
     public void atualizarConsulta(int id, boolean realizada) {
-        Consulta consulta = ConsultaService.buscarConsulta(id);
+        Consulta consulta = consultaService.buscarConsulta(id);
         if (consulta != null) {
             consulta.setRealizada(realizada);
-            ConsultaService.atualizarConsulta(consulta);
+            consultaService.atualizarConsulta(consulta);
         } else {
             System.out.println("Consulta com ID " + id + " não encontrada.");
         }
     }
 
     public void excluirConsulta(int id) {
-        Consulta consulta = ConsultaService.buscarConsulta(id);
+        Consulta consulta = consultaService.buscarConsulta(id);
         if (consulta != null) {
-            ConsultaService.excluirConsulta(id);
+            consultaService.excluirConsulta(id);
             System.out.println("Consulta com ID " + id + " excluída com sucesso.");
         } else {
             System.out.println("Consulta com ID " + id + " não encontrada.");
         }
-    }
-
-    public static void MarcarConsulta(int idAnimal, int idVeterinario, Date data) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'MarcarConsulta'");
     }
 }
